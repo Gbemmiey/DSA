8b3a2df1da965a4a12915f7e38ae8501844bc492
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2026-01-31 03:12:20 +0100

    chore: created method signature

--- Binary Search/C#/SimpleBinarySearch.cs
+++ Binary Search/C#/SimpleBinarySearch.cs
@@ -1,1 +1,15 @@
-Console.WriteLine("Hello World");
+/// <summary>
+/// Executes a simple binary search
+/// </summary>
+/// <param name="orderedArray">An ordered array of integers.</param>
+/// <param name="searchTerm">The integer being searched.</param>
+/// <returns>An integer for the index of the search Term.</returns>
+int ExecuteBinarySearch(int[] orderedArray, int searchTerm)
+{
+
+
+
+    return searchTerm;
+}
+
+Console.WriteLine(ExecuteBinarySearch([], 1));
